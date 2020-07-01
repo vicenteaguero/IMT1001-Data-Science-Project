acd3a2dcd0832c0e259cefab9347da2977e3fd86
Vicente Agüero <vicenteaguero@uc.cl>
2020-06-30 20:16:33 -0400

    AVANCE MARTES

--- Módulos/EMNIST.py
+++ Módulos/EMNIST.py
@@ -1,10 +1,18 @@
-import numpy as np
+import tensorflow_datasets as tdfs
 import pandas as pd
-df_train = pd.read_csv("EMNIST\emnist-byclass-train.csv", names = np.arange(-1,784))
-df_train = df_train.rename({-1:"labels"}, axis="columns")
-df_test = pd.read_csv("EMNIST\emnist-byclass-test.csv", names = np.arange(-1,784)) 
-df_test = df_test.rename({-1:"labels"}, axis="columns")
-y_train= np.array(df_train['labels'])
-x_train= np.array(df_train.drop('labels', axis=1))
-x_test= np.array(df_test.drop('labels', axis=1))
-y_test= np.array(df_test['labels'])
+data_train = tdfs.load("emnist", split="train")
+data_test = tdfs.load("emnist", split="test")
+data_train_numpy = tdfs.as_numpy(data_train)
+data_test_numpy = tdfs.as_numpy(data_test)
+dataframe_train = pd.DataFrame(data_train_numpy)
+dataframe_test = pd.DataFrame(data_test_numpy)
+x_train = dataframe_train["image"].to_numpy()
+y_train = dataframe_train["label"].to_numpy()
+x_test = dataframe_test["image"].to_numpy()
+y_test = dataframe_test["label"].to_numpy()
+for image in range (len(x_train)):
+  x_train[image] = x_train[image].reshape(28, 28).transpose()
+  x_train[image] = x_train[image].reshape(784)
+for image in range (len(x_test)):
+  x_test[image] = x_test[image].reshape(28, 28).transpose()
+  x_test[image] = x_test[image].reshape(784)
